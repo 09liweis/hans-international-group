1f648e8eea021ccd44ad33912f1d422c11ebd572
SiriYeung <119325339+SiriYeung@users.noreply.github.com>
2026-05-25 17:33:34 +0000

    refactor company name

---
+++ src/constants.ts
@@ -0,0 +1,21 @@
+// Company Constants
+export const COMPANY_NAME = "Hans Investment Group Ltd";
+export const COMPANY_TAGLINE = "Leading the Steel Industry with Excellence";
+export const COMPANY_DESCRIPTION = `Leading the Steel Industry with Excellence. ${COMPANY_NAME} provides high-quality steel products and services worldwide.`;
+export const COMPANY_URL = "https://www.hansig.ca/";
+export const COMPANY_LOGO_URL = "https://www.hansig.ca/favicon.png";
+
+// Address
+export const COMPANY_ADDRESS = {
+  street: "6 Sangster Road",
+  city: "Uxbridge",
+  province: "ON",
+  postal: "L9P 0G5",
+  country: "CA",
+};
+
+// Coordinates
+export const COMPANY_COORDINATES = {
+  latitude: "43.9066458",
+  longitude: "-79.6407358",
+};
